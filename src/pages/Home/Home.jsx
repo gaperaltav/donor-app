b86be179a608e9a3f45f99e28c001123992946f5
gaperaltav <gaperaltav@gmail.com>
2020-01-15 21:14:25 -0800

    Renaming donationLisnt import in Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar } from 'components';
 import { Container } from '@material-ui/core';
-import DonationList from 'containers/DonationList/DonationList';
+import DonationList from 'containers/donationList';
 
 const Home = () => {
   return (
