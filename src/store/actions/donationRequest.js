570c12d73eb5d80677539a631ad8ee88f10991e8
Gabriel Peralta <gaperaltav@gmail.com>
2020-02-11 19:12:36 -0400

    Adding actionTypes and actionCreator for create donation and reset donationRequest state

--- src/store/actions/donationRequest.js
+++ src/store/actions/donationRequest.js
@@ -1,7 +1,18 @@
+import api from '../../services/api';
 import {
   UPDATE_DONATION_REQUEST_TITLE,
   UPDATE_DONATION_REQUEST_DESCRIPTION,
+  UPDATE_DONATION_REQUEST_BLOODTYPE,
+  CREATE_DONATION_REQUEST_SUCCEED,
+  RESET_DONATION_REQUEST_INITIAL_STATE,
 } from '../actionTypes';
+
+export const resetDonationRequestToinitalState = () => (dispatch) => {
+  dispatch({
+    type: RESET_DONATION_REQUEST_INITIAL_STATE,
+    payload: null,
+  });
+};
 
 export const updateDonationRequestTitle = (title) => (dispatch) => {
   if (title.trim() === '') return;
@@ -12,10 +23,34 @@ export const updateDonationRequestTitle = (title) => (dispatch) => {
   });
 };
 
-export const updateDontaionRequestDescription = (description)=> (dispatch) => {
+export const updateDontaionRequestDescription = (description) => (dispatch) => {
   if (description.trim() === '') return;
   dispatch({
     type: UPDATE_DONATION_REQUEST_DESCRIPTION,
     payload: description,
   });
+};
+
+export const updateDontaionRequestBloodType = (bloodType) => (dispatch) => {
+  dispatch({
+    type: UPDATE_DONATION_REQUEST_BLOODTYPE,
+    payload: bloodType,
+  });
+};
+
+export const createDonationRequest = (donation) => async (dispatch) => {
+  const payload = {
+    ...donation,
+    //TODO: get the doneedID by the current user
+    bloodDoneeId: '5d2bc2680ef58a2f8a24d438'
+  };
+
+  const result = await api.newDonation(payload);
+
+  if (result.data) {
+    dispatch({
+      type: CREATE_DONATION_REQUEST_SUCCEED,
+      payload: result.data,
+    });
+  }
 };
