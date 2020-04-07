08d1d35ac90bb6bc232bededddfa84befc33809d
Gabriel Peralta <gaperaltav@gmail.com>
2020-04-07 00:49:16 -0400

    Creating DonationDeatil Page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router';
 import { Switch } from 'react-router-dom';
-import { Home, NotFound } from 'pages';
+import { Home, NotFound, DonationDetail } from 'pages';
 
 
 const AppRoutes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/detail" component={DonationDetail} />
+
       <Route component={NotFound}/>
     </Switch>
   );
