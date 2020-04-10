4cfd19235f2df380cce5be3a9590a40a627d782b
Gabriel Peralta <gaperaltav@gmail.com>
2020-04-10 02:31:06 -0400

    Changing donationTable functional component and fixing key index error

--- src/components/donationsTable/DonationsTable.jsx
+++ src/components/donationsTable/DonationsTable.jsx
@@ -1,55 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
 import { propTypes } from './props';
 import style from './style';
 import { Table, TableHead, TableRow, TableCell, TableBody, Paper, Toolbar, Typography, IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 
-class DonationsTable extends Component {
+const DonationsTable = (props) => {
 
-  onClickAddButton = () => {
+  const onClickRowHandler = () => {
 
-  }
+  };
 
-  render() {
-    const {
-      donations,
-      onClickAdd
-    } = this.props;
+  const {
+    donations,
+    onClickAdd,
+  } = props;
 
-    return (
-      <Paper style={style.paper} >
-        <Toolbar >
-          <Typography variant="h6" id="tableTitle">
-            Donor requests
+  return (
+    <Paper style={style.paper} >
+      <Toolbar >
+        <Typography variant="h6" id="tableTitle">
+          Donor requests
         </Typography>
-          <IconButton onClick={onClickAdd} aria-label="add">
-            <AddIcon />
-          </IconButton>
-        </Toolbar>
-        <Table style={style.Table} >
-          <TableHead>
-            <TableRow>
-              <TableCell>Blood type</TableCell>
-              <TableCell align="right">Title</TableCell>
-              <TableCell align="right">Created</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {donations && donations.map((donation) =>
-              <TableRow hover={true} key={donation.id} >
+        <IconButton onClick={onClickAdd} aria-label="add">
+          <AddIcon />
+        </IconButton>
+      </Toolbar>
+      <Table style={style.Table} >
+        <TableHead>
+          <TableRow>
+            <TableCell>Blood type</TableCell>
+            <TableCell align="right">Title</TableCell>
+            <TableCell align="right">Created</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {donations && donations.map(donation =>
+            <React.Fragment key={donation._id}>
+              <TableRow hover={true} onClick={onClickRowHandler} >
                 <TableCell key={donation.bloodType} component="th" scope="row">
                   {donation.bloodType}
                 </TableCell>
                 <TableCell key={donation.title} align="right">{donation.title}</TableCell>
                 <TableCell key={donation.created_at} align="right">{donation.created_at}</TableCell>
               </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </Paper>
-    );
-  }
-}
+            </React.Fragment>
+          )}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+};
 
 DonationsTable.propTypes = propTypes;
 
