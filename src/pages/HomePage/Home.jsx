bf21a703e6299b4d28034c2a0716a2ca1d69c08c
Gabriel Peralta <gaperaltav@gmail.com>
2020-05-09 01:37:38 -0400

    Fixing donationList container name in Home.jsx

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar } from 'components';
 import { Container } from '@material-ui/core';
-import DonationList from 'containers/DonationList';
+import DonationList from 'containers/donationList';
 
 const HomePage = () => {
   return (
