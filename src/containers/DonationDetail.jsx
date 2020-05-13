e9132cda54a56a2efe0fbd8396e1fd120709c8ac
Gabriel Peralta <gaperaltav@gmail.com>
2020-05-13 00:38:00 -0400

    Creating DonationDetail conainer

--- src/containers/DonationDetail.jsx
+++ src/containers/DonationDetail.jsx
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
 export default class DonationDetail extends Component {
-  
-};
+
+  componentDidMount = () => {
+
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
--- src/pages/DonationDetailPage.jsx
+++ src/pages/DonationDetailPage.jsx
@@ -1,15 +1,15 @@
 import React, { Fragment } from 'react';
 import { Container } from '@material-ui/core';
 import Navbar from 'components/Navbar';
-
+import DonationDetail from 'containers/DonationDetail';
 
 const DonationDetailPage = () => (
-    <Fragment>
-        <Navbar />
-        <Container>
-
-        </Container>
-    </Fragment>
+  <Fragment>
+    <Navbar />
+    <Container>
+      <DonationDetail />
+    </Container>
+  </Fragment>
 );
 
 export default DonationDetailPage;
